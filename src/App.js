3b611e9f40dc9c55ce6e84484f0190046a49ecba
Michel Kabwe <mikael.tallbo@webexpress.se>
2022-09-28 21:23:56 +0200

    modifiyed import path

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {useState, useEffect} from 'react'; 
-import {db} from './firebase-config';
+import {db} from '../firebase-config';
 import {collection, getDocs} from 'firebase/firestore';
 import './App.css';
 
